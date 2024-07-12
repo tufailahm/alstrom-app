e01c36674229f46dfe893a569614f0e2d1696610
Mohammad Tufail Ahmed <tufailahmedkhan@gmail.com>
2024-07-12 12:41:25 +0530

    changes done to fail the tests

--- src/test/java/com/training/bms/AlstromAppApplicationTests.java
+++ src/test/java/com/training/bms/AlstromAppApplicationTests.java
@@ -1,13 +1,14 @@
 package com.training.bms;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+
 class AlstromAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(190, 200);
 	}
 
 }
